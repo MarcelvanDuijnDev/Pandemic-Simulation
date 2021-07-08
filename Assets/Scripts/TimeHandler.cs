bfbd85b54a29a5a70093ce3de59316451ccee899
Marcel van Duijn <epicmars264@gmail.com>
2021-07-08 02:39:33 +0200

    V0.0.1
    Changes:
    * Added TextMesh Pro
    * Updated TimeHandler script

--- Assets/Scripts/TimeHandler.cs
+++ Assets/Scripts/TimeHandler.cs
@@ -1,41 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TimeHandler : MonoBehaviour
 {
-    public float CurrentDate = 0;
-    public Vector3 StartTime = new Vector3(3,16);
+    [Header("Settings")]
+    public Vector3 TimeSpeed = new Vector3(0,0,1);
+    public Vector3 StartTime = new Vector3(3,16,20);
     public Vector3 StartDate = new Vector3(7,7,2021);
 
+    [Header("TextMeshPro")]
+    [SerializeField] private TextMeshProUGUI _DateTimeText = null;
+
+    private Vector3 _CurrentTime;
+    private Vector3 _CurrentDate;
 
     void Start()
     {
-        CurrentDate = 0;
-
-        //Year/Month/Day
-        CurrentDate += StartDate.z * 31449600;
-        CurrentDate += StartDate.y * 2620800;
-        CurrentDate += StartDate.x * 86400;
-
-        //Hour/Minute/Seconds
-        CurrentDate += StartTime.x * 3600;
-        CurrentDate += StartTime.y * 60;
-        CurrentDate += StartTime.z;
-
-
-        float year = Mathf.FloorToInt(CurrentDate / 31449600);
-        float month = Mathf.FloorToInt((CurrentDate - (year * 31449600)) / 2620800);
-        float day = Mathf.FloorToInt((CurrentDate - (year * 31449600) - (month * 2620800)));
-
-
-
-        Debug.Log(year);
-        Debug.Log(month);
-        Debug.Log(day);
+        _CurrentTime = StartTime;
+        _CurrentDate = StartDate;
     }
 
     void Update()
     {
+        //Update Time
+        _CurrentTime.x += TimeSpeed.x * Time.deltaTime;
+        _CurrentTime.y += TimeSpeed.y * Time.deltaTime;
+        _CurrentTime.z += TimeSpeed.z * Time.deltaTime;
+
+
+        //Update Time
+        if (_CurrentTime.z > 60) //Seconds > Minutes
+        {
+            _CurrentTime.z = 0;
+            _CurrentTime.y += 1;
+        }
+        if(_CurrentTime.y > 60) //Minutes > Hours
+        {
+            _CurrentTime.y = 0;
+            _CurrentTime.x += 1;
+        }
+        if(_CurrentTime.x >= 24) //Hours > Days
+        {
+            _CurrentTime.x = 0;
+            _CurrentDate.x += 1;
+        }
+
+        //Update Date
+        //DAYS > MONTHS
+        if(_CurrentDate.y == 1 || _CurrentDate.y == 3 || _CurrentDate.y == 5 || _CurrentDate.y == 7 || _CurrentDate.y == 8 || _CurrentDate.y == 10 || _CurrentDate.y == 12) //31 days
+        {
+            if(_CurrentDate.x > 31)
+            {
+                _CurrentDate.x = 1;
+                _CurrentDate.y += 1;
+            }
+        }
+        if(_CurrentDate.y == 4 || _CurrentDate.y == 6 || _CurrentDate.y == 9 || _CurrentDate.y == 11) //30 days
+        {
+            if (_CurrentDate.x > 30)
+            {
+                _CurrentDate.x = 1;
+                _CurrentDate.y += 1;
+            }
+        }
+        if(_CurrentDate.y == 2) // 28 days (leap years not implemented)
+        {
+            if (_CurrentDate.x > 28)
+            {
+                _CurrentDate.x = 1;
+                _CurrentDate.y += 1;
+            }
+        }
+        //MONTHS > YEARS
+        if(_CurrentDate.y > 12)
+        {
+            _CurrentDate.y = 1;
+            _CurrentDate.z += 1;
+        }
+
+        //Display time/date
+        _DateTimeText.text = "Current Time: " + Mathf.Floor(_CurrentTime.x) + ":" + Mathf.Floor(_CurrentTime.y) + ":" + Mathf.Floor(_CurrentTime.z) + 
+            " / " + _CurrentDate.x + "- " + _CurrentDate.y + "- " + _CurrentDate.z;
     }
 }
